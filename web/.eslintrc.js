ebf5181248e334a155c13496d1201f2daf466c2a
Christian Blom <christian@christianblom.se>
2020-05-18 14:55:19 +0200

    Some tweaks and customizations.

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,5 +1,22 @@
 module.exports = {
-  extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
+  extends: [
+    'standard',
+    'standard-react',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    "browser": true,
+    "jasmine": true,
+    "jest": true,
+    "node": true
+  },
   rules: {
     'react/prop-types': 0,
     'object-curly-spacing': ['error', 'never']
